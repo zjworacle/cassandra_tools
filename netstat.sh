eae0ef409cd858e08bd78395a5d6d058fb3a69eb
Jinwen Zou <zjworacle@gmail.com>
2016-10-10 19:41:29 -0700

    Update netstat.sh

--- netstat.sh
+++ netstat.sh
@@ -1,10 +1,11 @@
 It's very command task to add/decommission/replace nodes in cassandra. Frequently asked question in operation is how  
 bootstrap/unbootstrap progess is going, what the ETA is, etc. 
 
-Cassandra has a built-in nodetool netstats to show where the data came from or sent to in files level, but its output is way to long
-for big cluster. 
+Cassandra has a built-in nodetool netstats to show where the data came from or sent to in files level, but its output
+is way to long for big cluster. 
 
-Here is a simple shell/AWK script to format nodetool netstats output to make it more readable, most importantly it calculates progress.
+Here is a simple shell/AWK script to format nodetool netstats output to make it more readable, most importantly it 
+calculates progress.
 
 
 #!/bin/bash
